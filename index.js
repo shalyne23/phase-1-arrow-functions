24a91ffb68cb56855d9f7b17256e23c4c75ead2d
shalyne23 <shalynelangat@gmail.com>
2024-07-01 08:14:01 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function divide(){
+    let a=2000
+    let b=100
+    let result=a/b
+    return result
+}
+console.log(divide(2000,100))
+const square=(a)=>{
+   return a*a;
+}
+const add=(a,b)=>{
+return a+b;
+}
